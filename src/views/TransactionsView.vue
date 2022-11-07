e75c0f32ad1b96395e783779daec6d323cc56695
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-11-07 11:51:07 -0300

    transactions filter by month

--- src/views/TransactionsView.vue
+++ src/views/TransactionsView.vue
@@ -1,23 +1,38 @@
 <template>
     <h2>Transactions view</h2>
+    <input v-model="monthFilter" type="month">
+    {{monthFilter}}
+
     <pre>
     {{ transactions }}
   </pre>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import api from '../config/axios.js'
 import { onMounted } from 'vue'
 
+const monthFilter = ref('')
+
+watch(monthFilter, async () => {
+    const [ year, month ] = monthFilter.value.split('-')
+    console.log(year, month)
+
+    await getTransactions(year, month)
+
+})
+
 const transactions = ref([])
 
-async function getTransactions() {
+async function getTransactions(year: string, month: string) {
+    console.log(year, month)
   return api.guiabolsoApi({
     method: 'get',
     url: `/hello`,
   }).then(function (response) {
     console.log(response.data)
+    transactions.value = response.data
     return response.data
   }).catch(function (error) {
     console.log(error.response?.data);
@@ -25,11 +40,19 @@ async function getTransactions() {
 }
 
 onMounted(async () => {
-  try {
-    transactions.value = await getTransactions()
-  } catch(err) {
-    console.log(err)
-  }    
+  const currentDate = new Date()
+  const year = currentDate.getFullYear()
+  const month = currentDate.getMonth() + 1
+
+  // console.log(`${year}-0${month}`)
+
+  monthFilter.value = `${year}-${month.toString().padStart(2, '0')}`
+
+//   try {
+//     transactions.value = await getTransactions()
+//   } catch(err) {
+//     console.log(err)
+//   }    
 })
 
 </script>
