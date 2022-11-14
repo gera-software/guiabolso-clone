2f4528330b05b2fb5a877f2d2f4bb34d0317e10e
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-11-14 18:48:53 -0300

    account list style

--- src/views/AccountsView.vue
+++ src/views/AccountsView.vue
@@ -5,6 +5,52 @@
     </router-link>
     <h1>Contas e Cartões</h1>
   </div>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">Nubank</div>
+      <div class="account">
+        <div class="name">cartão 5346</div>
+        <div class="balance">R$ -3645,88</div>
+      </div>
+      <div class="account">
+        <div class="name">cartão 5346</div>
+        <div class="balance">R$ -3645,88</div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">Nubank</div>
+      <div class="account">
+        <div class="name">cartão 5346</div>
+        <div class="balance">R$ -3645,88</div>
+      </div>
+      <div class="account">
+        <div class="name">cartão 5346</div>
+        <div class="balance">R$ -3645,88</div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">Nubank</div>
+      <div class="account">
+        <div class="name">cartão 5346</div>
+        <div class="balance">R$ -3645,88</div>
+      </div>
+      <div class="account">
+        <div class="name">cartão 5346</div>
+        <div class="balance">R$ -3645,88</div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">Nubank</div>
+      <div class="account">
+        <div class="name">cartão 5346</div>
+        <div class="balance">R$ -3645,88</div>
+      </div>
+      <div class="account">
+        <div class="name">cartão 5346</div>
+        <div class="balance">R$ -3645,88</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -24,6 +70,9 @@ import { groupBy } from 'lodash'
   justify-content: flex-start;
   align-items: center;
   padding: 15px 20px;
+  position: fixed;
+  top: 0;
+  width: 100vw;
 }
 
 .app-bar .icon {
@@ -38,6 +87,47 @@ import { groupBy } from 'lodash'
   margin-left: 40px;
 }
 
+.container {
+  margin-top: 60px;
+}
+
+.card {
+  background-color: white;
+  margin: 30px 0;
+  padding: 20px;
+  padding-bottom: 0;
+}
+
+.card-header {
+  font-weight: 600;
+  color: #404040;
+  font-size: 22px;
+  margin-bottom: 15px;
+}
+
+.account {
+  /* background-color: #F9386A; */
+  border-bottom: 1px solid #F2F2F2;
+  padding: 15px 10px;
+}
+
+.account:last-child {
+  border-bottom: none;
+}
+
+.account .name {
+  font-size: 12px;
+  font-weight: 400;
+  color: #404040;
+  text-transform: uppercase;
+}
+
+.account .balance {
+  font-family: 'Axiforma';
+  font-size: 30px;
+  font-weight: 800;
+  color: #404040;
+}
 
 </style>
   
