99b97bcea63a22dbd64862a13fbc9666de051c74
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-11-15 20:57:24 -0300

    TransactionSummary Component

---
+++ src/components/TransactionSummary.vue
@@ -0,0 +1,133 @@
+<template>
+    <div class="transaction">
+        <div class="col-1">
+            <CategoryIcon :icon="transaction.category?.iconName" :color="transaction.category?.primaryColor" />
+            <div class="flex">
+                <span class="category">{{
+                    transaction.category?.name
+                }}</span>
+                <span class="description">{{
+                    transaction.description
+                }}</span>
+            </div>
+        </div>
+        <div class="col-2">
+            <span class="account">{{
+                transaction.account.name
+            }}</span>
+            <span class="value">R$ {{ (+transaction.amount / 100).toFixed(2) }}</span>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import CategoryIcon from '@/components/CategoryIcon.vue'
+
+
+interface Category {
+    _id?: String,
+    name: String,
+    iconName: String,
+    primaryColor: String
+}
+
+enum CurrencyCodes {
+    BRL = 'BRL',
+}
+
+enum TransactionType {
+    EXPENSE = 'EXPENSE',
+    INCOME = 'INCOME',
+}
+
+enum TransactionStatus {
+    PENDING = 'PENDING',
+    POSTED = 'POSTED',
+}
+
+enum AccountType {
+    WALLET = 'WALLET',
+    BANK = 'BANK',
+    CREDIT_CARD = 'CREDIT_CARD'
+}
+
+interface AccountSummary {
+  _id?: String,
+  name: String,
+  type: AccountType,
+  imageUrl?: String,
+}
+
+interface TransactionSummary {
+    _id?: String,
+    description: String,
+    amount: Number,
+    currencyCode: CurrencyCodes,
+    date: Date,
+    category?: Category,
+    type: TransactionType,
+    status: TransactionStatus,
+    ignored: Boolean,
+    account: AccountSummary
+}
+
+
+const props = defineProps<{
+    transaction: TransactionSummary
+}>()
+</script>
+<style scoped>
+
+.transaction {
+    display: flex;
+    justify-content: space-between;
+    padding: 15px 0;
+}
+
+.transaction .col-1 {
+    display: flex;
+    /* flex-direction: column; */
+    flex-direction: row;
+    flex-basis: min-content;
+    overflow: hidden;
+  }
+
+.transaction .col-2 {
+    display: flex;
+    flex-direction: column;
+    text-align: right;
+    flex-shrink: 0;
+  }
+
+.transaction .flex {
+    padding-left: 20px;
+    display: flex;
+    flex-direction: column;
+    flex-basis: min-content;
+    overflow: hidden;
+  }
+
+.transaction .category {
+    font-size: .8em;
+    color: #454545;
+    white-space: nowrap;
+  }
+
+.transaction .description {
+    font-size: 1em;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    color: #222222;
+  }
+
+.transaction .account {
+    font-size: .8em;
+    color: #454545;
+  }
+
+.transaction .value {
+    font-size: 1em;
+    font-weight: bold;
+    color: #222222;
+}
+</style>
--- src/views/ExtractView.vue
+++ src/views/ExtractView.vue
@@ -19,25 +19,7 @@
             </div>
           </div>
 
-          <div class="transaction">
-              <div class="col-1">
-                  <CategoryIcon :icon="transaction.category?.iconName" :color="transaction.category?.primaryColor" />
-                  <div class="flex">
-                      <span class="category">{{
-                          transaction.category?.name
-                      }}</span>
-                      <span class="description">{{
-                          transaction.description
-                      }}</span>
-                  </div>
-              </div>
-              <div class="col-2">
-                  <span class="account">{{
-                      transaction.account.name
-                  }}</span>
-                  <span class="value">R$ {{ (+transaction.amount / 100).toFixed(2) }}</span>
-              </div>
-          </div>
+          <TransactionSummary :transaction="transaction"></TransactionSummary>
         </div>
 
       </div>
@@ -48,9 +30,8 @@
 import { ref, watch, computed } from "vue";
 import api from "../config/axios.js";
 import { onMounted } from "vue";
-import { groupBy, sortBy, toPairs, orderBy } from "lodash";
 import { useRoute } from "vue-router";
-import CategoryIcon from '@/components/CategoryIcon.vue'
+import TransactionSummary from "../components/TransactionSummary.vue";
 
 
 const route = useRoute()
@@ -226,57 +207,4 @@ Transaction
     text-transform: uppercase;
 }
 
-.transactions-container .transaction {
-    display: flex;
-    justify-content: space-between;
-    padding: 15px 0;
-}
-
-.transactions-container .transaction .col-1 {
-    display: flex;
-    /* flex-direction: column; */
-    flex-direction: row;
-    flex-basis: min-content;
-    overflow: hidden;
-  }
-
-.transactions-container .transaction .col-2 {
-    display: flex;
-    flex-direction: column;
-    text-align: right;
-    flex-shrink: 0;
-  }
-
-  .transactions-container .transaction .flex {
-    padding-left: 20px;
-    display: flex;
-    flex-direction: column;
-    flex-basis: min-content;
-    overflow: hidden;
-  }
-
-  .transactions-container .transaction .category {
-    font-size: .8em;
-    color: #454545;
-    white-space: nowrap;
-  }
-
-  .transactions-container .transaction .description {
-    font-size: 1em;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    color: #222222;
-  }
-
-  .transactions-container .transaction .account {
-    font-size: .8em;
-    color: #454545;
-  }
-
-  .transactions-container .transaction .value {
-    font-size: 1em;
-    font-weight: bold;
-    color: #222222;
-  }
 </style>
