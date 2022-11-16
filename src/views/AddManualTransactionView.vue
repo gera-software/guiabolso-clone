41970b9265bbabcbe5d6d0e5eab3d6fbfe42900e
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-11-16 14:00:10 -0300

    Add manual transaction: default to current date

--- src/views/AddManualTransactionView.vue
+++ src/views/AddManualTransactionView.vue
@@ -1,33 +1,35 @@
 <template>
     <AppBar title="Nova transação manual" />
     <div class="container">
-        <div class="form-group">
-            <label class="form-label">Nome da Transação</label>
-            <input class="form-input" type="text" placeholder="Crie um nome para a transação">
-        </div>
-        <div class="form-group">
-            <label class="form-label">Valor</label>
-            <input class="form-input" type="text" placeholder="0.00">
-        </div>
-        <div class="form-group">
-            <label class="form-label">Data</label>
-            <input class="form-input" type="date">
-        </div>
-        <div class="form-group">
-            <label class="form-label">Conta</label>
-            <input class="form-input" type="text">
-        </div>
-        <div class="form-group">
-            <label class="form-label">Categoria</label>
-            <input class="form-input" type="text">
-        </div>
-        <div class="form-group">
-            <label class="form-label">Comentários e #tags</label>
-            <input class="form-input" type="text" placeholder="Escreva seus comentários e #tags">
-        </div>
-        <div class="form-group">
-            <button class="button">Salvar</button>
-        </div>
+        <form @submit.prevent="handleSubmit">
+            <div class="form-group">
+                <label class="form-label">Nome da Transação</label>
+                <input class="form-input" type="text" placeholder="Crie um nome para a transação" required v-model="form.description">
+            </div>
+            <div class="form-group">
+                <label class="form-label">Valor</label>
+                <input class="form-input" type="text" placeholder="0.00" required v-model="form.value">
+            </div>
+            <div class="form-group">
+                <label class="form-label">Data</label>
+                <input class="form-input" type="date" required v-model="form.date">
+            </div>
+            <div class="form-group">
+                <label class="form-label">Conta</label>
+                <input class="form-input" type="text" required v-model="form.account">
+            </div>
+            <div class="form-group">
+                <label class="form-label">Categoria</label>
+                <input class="form-input" type="text" v-model="form.category">
+            </div>
+            <div class="form-group">
+                <label class="form-label">Comentários e #tags</label>
+                <input class="form-input" type="text" placeholder="Escreva seus comentários e #tags" v-model="form.comment">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="button">Salvar</button>
+            </div>
+        </form>
     </div>
 </template>
 
@@ -82,6 +84,20 @@ onMounted(async () => {
 })
 
 
+const form = ref({
+    description: 'testando',
+    value: 50.78,
+    date: (new Date()).toISOString().split('T')[0],
+    account: 'sadsad',
+    category: 'sdasd',
+    comment: '#paz',
+})
+
+
+function handleSubmit(e: any) {
+    console.log(e)
+    console.log(form.value)
+}
 </script>
 
 <style scoped>
