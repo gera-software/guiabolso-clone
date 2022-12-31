1aefda5e34cfc0649483a9a47512edf2a8563db0
Gilmar Andrade <gilmar-andrade@outlook.com>
2022-12-31 16:43:49 -0300

    vite pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,42 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteCommonjs(), vue()],
+  plugins: [
+    viteCommonjs(), 
+    vue(),
+    VitePWA({ 
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'vite.svg'],
+      manifest: {
+        name: 'Guiabolso Clone',
+        short_name: 'Guiabolso',
+        description: 'Clone do App Guiabolso porque o original foi descontinuado',
+        theme_color: '#ff0000',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      devOptions: {
+        enabled: true
+      }
+     }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
