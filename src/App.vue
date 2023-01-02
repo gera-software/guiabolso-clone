563f3a5edda78aa926dfa9dd3b9fe9bccb74ad67
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-01-01 22:38:27 -0300

    check online, offline

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <!-- <ul style="margin:0">
     <li>
@@ -13,6 +9,35 @@
   </ul> -->
   <router-view></router-view>
 </template>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+// @ts-ignore
+import Toastify from 'toastify-js'
+
+onMounted(() => {
+  window.addEventListener('offline', () => {
+    // Update your UI to reflect that there's no connection.
+    // window.alert('you are offline')
+    Toastify({
+            duration: 5000,
+            text: `<h4 style='display: inline'>Você está offline mesmo...</h4> `,
+            escapeMarkup: false,
+            gravity: "top",
+          }).showToast();
+  });
+  
+  window.addEventListener('online', () => {
+    // Update your UI to reflect that the connection is back.
+    // window.alert('you are online')
+    Toastify({
+            duration: 5000,
+            text: `<h4 style='display: inline'>Agora tá de volta online</h4> `,
+            escapeMarkup: false,
+            gravity: "top",
+          }).showToast();
+  });
+})
+</script>
 
 <style scoped>
 </style>
