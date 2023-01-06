8a7f7b2a3c3d086f96227f81455f9ac536cd5fbd
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-01-05 22:45:53 -0300

    credit card transaction creation: date comparison

--- netlify/functions/transaction-create.ts
+++ netlify/functions/transaction-create.ts
@@ -34,21 +34,66 @@ const handler: Handler = async (event, context) => {
 
     const transaction = JSON.parse(event.body) as Transaction;
 
-    if(transaction.accountType == AccountType.CREDIT_CARD) {
-        console.log('CARTÃO', transaction.date, new Date(transaction.date).toLocaleDateString())
-    }
-
-    let doc: Transaction | null;
     try {
-        doc = await TransactionRepository.create(transaction);
-        const account = await AccountRepository.addToBalance(transaction.accountId.toString(), transaction.amount.valueOf());
-        console.log(account)
+        // TODO se é cartão de credito, verificar o fechamento e vencimento do cartão para definir em qual mês será lançada a transação
+        if(transaction.accountType == AccountType.CREDIT_CARD) {
+            const creditCardDate = new Date(transaction.date)
+            let invoiceMonth = creditCardDate.getMonth()
+            let invoiceYear = creditCardDate.getFullYear()
+
+            const invoiceDate = new Date(
+                invoiceYear, 
+                invoiceMonth, 
+                creditCardDate.getDate(), 
+                23, 59, 59);
+            console.log('INVOICE DATE', invoiceDate.toLocaleString())
+
+            const account = await AccountRepository.getById(transaction.accountId)
+            const closeDay = +(account?.creditData?.closeDay ?? 0)
+            const dueDay = +(account?.creditData?.dueDay ?? 0)
+            
+            const closingDate =  new Date(
+                invoiceYear, 
+                invoiceMonth, 
+                closeDay, 
+                0, 0, 0);
+            console.log('CLOSING DATE', closingDate.toLocaleString())
+
+            const dueDate = new Date(
+                invoiceYear, 
+                invoiceMonth, 
+                dueDay, 
+                23, 59, 59);
+            console.log('DUE DATE', dueDate.toLocaleString())
+
+
+
+
+            console.log('ACCOUNT', creditCardDate.toLocaleDateString(), closeDay, dueDay)
+            if(creditCardDate >= closingDate) {
+                console.log('dentro da fatura do mes que vem')
+            } else {
+                console.log('dentro da fatura do mes atual')
+            }
+        }
     } catch (err) {
         return {
             statusCode: 500,
             body: JSON.stringify(err),
         };
     }
+
+    // let doc: Transaction | null;
+    // try {
+    //     doc = await TransactionRepository.create(transaction);
+    //     const account = await AccountRepository.addToBalance(transaction.accountId.toString(), transaction.amount.valueOf());
+    //     console.log(account)
+    // } catch (err) {
+    //     return {
+    //         statusCode: 500,
+    //         body: JSON.stringify(err),
+    //     };
+    // }
 
     return {
         statusCode: 200,
